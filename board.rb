759b5d9b2463b94216e6528efa2923367fcf2319
Dan Kleiman <info@dankleiman.com>
2014-07-28 08:18:00 -0400

    Simple move through the board

--- board.rb
+++ board.rb
@@ -2,12 +2,14 @@ class Board < Array
   attr_reader :board
 
   def initialize
-    @board = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    @board = [0,0,0,-3,0,0,0,2,1,-4,0,-5,3,0,0]
   end
 
   def length
     @board.length
   end
 
-
+  def current_square_value(position)
+    @board[position]
+  end
 end
--- chutes_and_ladders.rb
+++ chutes_and_ladders.rb
@@ -7,15 +7,23 @@ puts "Welcome, #{player1.name}"
 board = Board.new
 dice = Dice.new
 
-puts board.length
-
 while player1.position < board.length
-  puts "#{player1.name} moves"
-  player1.position += dice.roll
-  if player1.position >= 15
-    puts "#{player1.name} wins!"
-  else
-    puts "New position: #{player1.position}"
+  moves = dice.roll
+  puts "#{player1.name} rolls #{moves}"
+  player1.position += moves
+
+  if player1.position < 15
+    current_square = board.current_square_value(player1.position)
+    puts "#{player1.name} lands on #{current_square}"
+    if current_square > 0
+      puts "Move ahead #{current_square}"
+    elsif current_square < 0
+      puts "Oh no, move back #{current_square}"
+    end
+    player1.position += current_square
+    puts "#{player1.name} is now on #{player1.position}"
   end
 end
+
+puts "#{player1.name} wins!"
 
